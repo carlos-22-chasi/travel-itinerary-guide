b1507ad27c74beb28ebb84717295f2c0f62a04e7
carlos-22-chasi <c071503s@gmail.com>
2024-07-16 17:23:48 -0500

    updated css

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-
+import './App.css';
 function App(){
   const [data, setData] = useState([{}])
 
@@ -15,7 +15,7 @@ function App(){
   }, []);
 
   return (
-    <div>
+    <div id = "square">
       {/* if data not loaded in, the have loading on screen, otherwise have each member in a p tag */}
       {(typeof data.members === 'undefined') ? (
         <p>Loading...</p>
@@ -24,6 +24,7 @@ function App(){
           <p key = {i}>{member}</p>
         ))
       )}
+      <p>fdafa</p>
     </div>
   )
 }
